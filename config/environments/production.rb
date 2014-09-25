3e1e1b8851a645820af7af53a3c6c681d548ae7b
Yaron <justame@gmail.com>
2014-09-25 22:44:43 +0300

    register

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,6 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.assets.debug = true
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
