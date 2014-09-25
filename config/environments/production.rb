23649e0d88f5eb691ac57cd7ae6116c14afe802e
Yaron <justame@gmail.com>
2014-09-25 21:51:22 +0300

    register

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,11 @@ Rails.application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = nil
+  # config.assets.js_compressor = :uglifier
+  
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
