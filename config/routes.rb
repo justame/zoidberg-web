a440af24814866e166ea2f7bc41761788ed14fc8
Yaron <justame@gmail.com>
2014-09-26 02:43:52 +0300

    register login logout is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  
+  # devise_for :users
   devise_for :users, :skip => [:sessions, :registrations]
   as :user do
     get 'login' => 'devise/sessions#new', :as => :new_user_session
     post 'login' => 'devise/sessions#create', :as => :user_session
     get 'register' => 'devise/registrations#new', :as => :new_user_registration
     post 'register' => 'devise/registrations#create', :as => :user_registration
+    get 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
