abc828a51e8b3ea3103e113f9bff440946944d40
Yaron <justame@gmail.com>
2014-09-28 03:18:40 +0300

    added likes users visible

--- app/controllers/like_tracks_controller.rb
+++ app/controllers/like_tracks_controller.rb
@@ -5,8 +5,8 @@ class LikeTracksController < ApplicationController
     like_tracks.each do |like_track|
       website_profile = WebsiteProfile.find(like_track.website_profile_id)
       user = {}
-      user[:main_image_url] = website_profile[:main_image_url]
-      user[:name] = website_profile[:name]
+      user[:main_image_url] = website_profile[:main_image_url].to_s.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => '?'})
+      user[:name] = website_profile[:name].to_s.encode('UTF-8', {:invalid => :replace, :undef => :replace, :replace => '?'})
       users.push(user)
     end
 
